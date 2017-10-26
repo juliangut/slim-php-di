6c8bfec66d7523abc5ab5678db6bad0a6e365ad9
Julián Gutiérrez <juliangut@gmail.com>
2017-10-27 00:07:08 +0200

    use interface in test

--- tests/PHPDI/ContainerTest.php
+++ tests/PHPDI/ContainerTest.php
@@ -151,7 +151,7 @@ class ContainerTest extends TestCase
 
         self::assertTrue($this->container->has('router'));
         self::assertInstanceOf(RouterInterface::class, $this->container->get('router'));
-        self::assertTrue($this->container->has(Router::class));
+        self::assertTrue($this->container->has(RouterInterface::class));
         self::assertInstanceOf(RouterInterface::class, $this->container->get(Router::class));
 
         self::assertTrue($this->container->has('phpErrorHandler'));
