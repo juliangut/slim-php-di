6ada0a7ce7ab7082627ec673fdd9d61d2c1e9c06
Julián Gutiérrez <juliangut@gmail.com>
2023-12-05 20:33:25 +0100

    update notice

--- ecs.php
+++ ecs.php
@@ -1,12 +1,10 @@
 <?php
 
 /*
- * slim-php-di (https://github.com/juliangut/slim-php-di).
- * Slim Framework PHP-DI container implementation.
+ * (c) 2015-2023 Julián Gutiérrez <juliangut@gmail.com>
  *
  * @license BSD-3-Clause
  * @link https://github.com/juliangut/slim-php-di
- * @author Julián Gutiérrez <juliangut@gmail.com>
  */
 
 declare(strict_types=1);
@@ -16,16 +14,14 @@ use PhpCsFixer\Fixer\ArrayNotation\ReturnToYieldFromFixer;
 use PhpCsFixer\Fixer\Basic\CurlyBracesPositionFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-$header = <<<'HEADER'
-slim-php-di (https://github.com/juliangut/slim-php-di).
-Slim Framework PHP-DI container implementation.
+return static function (ECSConfig $ecsConfig): void {
+    $header = <<<'HEADER'
+    (c) 2015-{{year}} Julián Gutiérrez <juliangut@gmail.com>
 
-@license BSD-3-Clause
-@link https://github.com/juliangut/slim-php-di
-@author Julián Gutiérrez <juliangut@gmail.com>
-HEADER;
+    @license BSD-3-Clause
+    @link https://github.com/juliangut/slim-php-di
+    HEADER;
 
-return static function (ECSConfig $ecsConfig) use ($header): void {
     $ecsConfig->paths([
         __FILE__,
         __DIR__ . '/src',
